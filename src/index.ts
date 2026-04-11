89b57d86a84f3222cc0b0d8682f34baf445efd1f
Diego Carlino <carlid.dev@gmail.com>
2026-04-11 21:58:08 +0200

    Export session embedding helpers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-export { loadConfig } from "./config/load";
-export type { SloppyConfig } from "./config/schema";
+export { expandHomePath, loadConfig, loadConfigFromPaths, normalizeConfig, readConfigFile } from "./config/load";
+export { writeHomeLlmConfig } from "./config/persist";
+export { sloppyConfigSchema } from "./config/schema";
+export type { LlmConfig, LlmProfileConfig, LlmProvider, RawSloppyConfig, SloppyConfig } from "./config/schema";
 export { Agent } from "./core/agent";
+export { LlmConfigurationError, LlmProfileManager } from "./llm/profile-manager";
+export { NodeSocketClientTransport } from "./providers/node-socket";
 export type { AgentSessionSnapshot } from "./session";
 export { AgentSessionProvider, SessionRuntime, SessionService, SessionStore } from "./session";
